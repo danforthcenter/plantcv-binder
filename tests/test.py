364bb57835d806989c6e4e2c3df2dc404057d9bc
Noah Fahlgren <noahfahlgren@gmail.com>
2021-07-09 13:12:33 -0500

    Use notebook list for tests

--- tests/test.py
+++ tests/test.py
@@ -18,29 +18,27 @@ def setup_function():
 # ##########################
 # Tests executing the notebook
 # ##########################
-inputs_list = [
-    ['.', 'index.ipynb'],
-    ['notebooks/color_correction_tutorial', 'color_correct_tutorial.ipynb'],
-    ['notebooks/hyperspectral_tutorial', 'hyperspectral_tutorial.ipynb'],
-    ['notebooks/input_output_tutorial', 'input_output.ipynb'],
-    ['notebooks/morphology_tutorial', 'morphology_tutorial.ipynb'],
-    ['notebooks/multi_plant_tutorial', 'multi_plant_tutorial.ipynb'],
-    ['notebooks/naive_bayes_tutorial', 'machine_learning.ipynb'],
-    ['notebooks/nir_tutorial', 'nir_tutorial.ipynb'],
-    ['notebooks/photosynthesis_tutorial', 'psII_tutorial.ipynb'],
-    ['notebooks/roi_tutorial', 'roi_package.ipynb'],
-    ['notebooks/thermal_tutorial', 'thermal.ipynb'],
-    ['notebooks/threshold_tutorial', 'threshold.ipynb'],
-    ['notebooks/vis_nir_tutorial', 'vis_nir_tutorial.ipynb'],
-    ['notebooks/vis_tutorial', 'vis_tutorial.ipynb'],
-    ['notebooks/watershed_segmentation_tutorial', 'segmentation.ipynb']
+notebook_list = [
+    'index.ipynb',
+    'notebooks/color_correction_tutorial/color_correct_tutorial.ipynb',
+    'notebooks/hyperspectral_tutorial/hyperspectral_tutorial.ipynb',
+    'notebooks/input_output_tutorial/input_output.ipynb',
+    'notebooks/morphology_tutorial/morphology_tutorial.ipynb',
+    'notebooks/multi_plant_tutorial/multi_plant_tutorial.ipynb',
+    'notebooks/naive_bayes_tutorial/machine_learning.ipynb',
+    'notebooks/nir_tutorial/nir_tutorial.ipynb',
+    'notebooks/photosynthesis_tutorial/psII_tutorial.ipynb',
+    'notebooks/roi_tutorial/roi_package.ipynb',
+    'notebooks/thermal_tutorial/thermal.ipynb',
+    'notebooks/threshold_tutorial/threshold.ipynb',
+    'notebooks/vis_nir_tutorial/vis_nir_tutorial.ipynb',
+    'notebooks/vis_tutorial/vis_tutorial.ipynb',
+    'notebooks/watershed_segmentation_tutorial/segmentation.ipynb'
 ]
 
-@pytest.mark.parametrize('dir,notebook', inputs_list)
-def test_notebook(dir, notebook, tmpdir):
+@pytest.mark.parametrize('notebook', notebook_list)
+def test_notebook(notebook, tmpdir):
     tmp = tmpdir.mkdir('sub')
-    # Change working directory
-    os.chdir(dir)
     # Open the notebook
     with open(notebook, "r") as f:
         nb = nbformat.read(f, as_version=4)
